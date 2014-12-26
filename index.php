a9a5569a7053e7b72e0e2f662224e6b5f9b14ad6
Christine Munar <chrismunar@gmail.com>
2014-12-26 00:56:16 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,188 @@
+
+<!DOCTYPE html>
+<?
+
+$page = 'home';
+
+?>
+
+<html>
+	<head>
+		<link rel="shortcut icon" href="images\favicon.ico">
+		<title>Loop</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom styles CSS -->
+		<link href="css/style.css" rel="stylesheet" media="screen">
+		<link rel="shortcut icon" href="favicon.ico">
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]-->
+		<script src="../../assets/js/html5shiv.js"></script>
+		<script src="../../assets/js/respond.min.js"></script>
+		<!--[endif]-->
+        
+	</head>
+	
+	<body>
+
+	<!--Navigation Bar-->
+		<div class="navbar navbar-default navbar-fixed-top header-nav" role="navigation">
+	  	<div class="container-fluid">
+	  	<!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+		        </button>
+			    <a class="navbar-brand" href="/">
+			      	<img src="images/LoopLogo.png" alt="Loop Logo" />
+			    </a>
+		    </div>
+			<div class="collapse navbar-collapse site-nav" role="navigation">
+			  <ul class="nav navbar-nav">
+			   	<li><a href="http://www.ekoapp.com/about">About Us</a></li>
+			   	<li><a href="http://www.ekoapp.com" target="_blank">Eko</a></li>
+				<li><a href="http://www.ekoapp.com/jobs">Contact</a></li>
+			  </ul>	
+			</div>
+	    </div>
+	</div>
+	<!---End of Navigation Bar-->
+
+
+	<!-- Main jumbotron for a primary marketing message or call to action -->
+	    <div class="jumbotron header-jumbotron">
+	      <div class="container">
+	      	<div class="row">
+	      		<div id="cta-title" class="col-sm-6 col-lg-5 col-lg-offset-1">
+			        <div class="text">
+						<img src="images/LoopLogoWords.png" class="img-responsive" alt="Loop">
+						<br><br>
+						<h3>Yes. No.<br> Get an answer. <br> Close the loop.</h3>
+						<br>
+					</div>
+
+				    
+					<p class="button-row">
+				      	<a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8", target="itunes_store" button type="button" class="btn btn-default btn-store"><img src="images/badge-store-ios.svg" height="30" width="30"> App Store</a>
+				      	<a href="https://play.google.com/store/apps/details?id=com.ekoapp.loop&hl=en" target="_blank" button type="button" class="btn btn-default btn-store pull-right"><img src="images/badge-store-play.svg" height="30" width="30"> Play Store</a>
+				    </p>
+				    </p>
+		        </div>
+
+		        
+		         <div class="col-sm-6 screenshot-device">
+		        	<img src="images/landing.png" class="img-responsive" alt="Responsive image">
+		        </div>
+		        
+
+		      </div>
+	    	</div>
+	    </div>
+
+
+		
+		<div class="section team full-panel hidden-xs">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-sm-7 screenshot-device3">
+					<p>&nbsp;</p>
+		        	<img src="images/2ndpanel.png" class="img-responsive" alt="Responsive image">
+						
+					</div>
+					<div class="col-sm-5 padded-text">
+						<h3>It's that simple.</h3>
+						<p>Saying yes or no has never been easier.<br>Add a message if you'd like.</p>
+						<p class="button-row">
+						<p>&nbsp;</p>
+				      <a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8", target="itunes_store" button type="button" class="btn btn-default btn-store"> Download For Free</a>
+					</p>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		
+		<!-- desktop section -->
+		<div class="section stickers full-panel hidden-xs">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-1 padded-text"></div>
+					<div class="col-sm-4 padded-text">
+						<h3>Loops are forever.</h3>
+						<p>All Loops are archived for easy access.<br>Never lose track of commitments again.
+							<p>&nbsp;</p>
+							<p class="button-row">
+				      		<a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8", target="itunes_store" button type="button" class="btn btn-default btn-store3">Get The App</a>
+							</p>
+					</div>
+
+					<div class="col-sm-7 screenshot-device2">
+						<img src="images/3rdpanel.png" class="img-responsive" alt="Responsive image">
+					</div>
+	
+				</div>
+			</div>
+		</div>
+<!-- FOOTER -->
+<footer id="footer">
+	<div class="row">
+		<p class="button-row">
+			<a href="https://www.facebook.com/LoopByEko" target="blank"><img src="images/fb.png" alt="facebook"></a>
+			<a href="https://www.twitter.com/LoopByEko" target="blank"><img src="images/twitter.png" alt="twitter"></a>
+			<a href="https://www.youtube.com/channel/UCpPqOkbaD5VWw2-DHbNJBDA" target="blank"><img src="images/youtube.png" alt="youtube"></a>
+			<a href="https://www.instagram.com/LoopByEko" target="blank"><img src="images/ig.png" alt="facebook"></a>
+			<a href="https://plus.google.com/u/0/b/108031800239412567632/108031800239412567632/posts " target="blank"><img src="images/gplus.png" alt="twitter"></a>
+			<a href="https://www.linkedin.com/company/loop-by-eko?trk=biz-brand-tree-co-name" target="blank"><img src="images/linkedin.png" alt="youtube"></a>
+		</p>
+	</div>
+	<br>
+	<p><small>Loop By Eko</small></p>
+	<span><a href="/eko.apk">Download Eko for Android</a></span>
+    <p><small><a href="privacy">Privacy</a> &nbsp;|&nbsp; <a href="terms">Terms</a> &nbsp;&nbsp; <a href="tutorial"></a></small></p>
+    <div class="container">
+		<span>Copyright Eko Communications, Inc</span>|
+		<span><a href="privacy">Privacy</a></span>|
+		<span><a href="terms">Terms</a></span>
+	</div>
+</footer>
+<!--END OF FOOTER-->
+
+<!--SCRIPTS-->
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="//code.jquery.com/jquery.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+
+	 <!-- custom JS -->
+	<script type="text/javascript" src="js/skrollr.min.js"></script>
+	<script type="text/javascript">
+	var s = skrollr.init();
+	</script>
+
+	<!-- Google Analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-42797895-2', 'ekoapp.com');
+	  ga('send', 'pageview');
+	</script>
+<!--END OF SCRIPTS-->
+
+	</body>
+</html>
